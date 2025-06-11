06811ac5fc449fa4e0fd1c2b16701387f7783f67
dholmes5247 <dholmes5247@hotmail.com>
2025-06-11 12:53:00 -0500

    Testing add method

--- Calculator/src/main/java/org/example/Main.java
+++ Calculator/src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        Calculator calculator = new Calculator();
+        int result = calculator.add(111, -11);
+        System.out.println("The result is: " + result + " !");
 
     }
 }
